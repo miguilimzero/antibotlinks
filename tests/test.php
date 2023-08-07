89bd4e2d3f91e17ad9c32a8ad386bf47332dd751
miguilim <35383529+srdante@users.noreply.github.com>
2023-08-07 19:51:34 -0300

    Update tests

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,10 @@
 <?php
 
-require 'bootstrap.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Miguilim\AntiBotLinks\AntiBotLinks;
+use Miguilim\AntiBotLinks\CacheAdapters\SimpleFileCacheAdapter;
 
-$antiBotLinks = AntiBotLinks::make('1', 60);
+$antiBotLinks = AntiBotLinks::make('1', new SimpleFileCacheAdapter(__DIR__ . '/cache'), 60);
 
 var_dump($antiBotLinks->generateLinks());
